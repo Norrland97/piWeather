84a0b3188c4e364721dbff4bfd79533b27889074
norrisarpi <fredholm97@gmail.com>
2020-12-22 23:50:11 +0100

    fixes

--- pi-weather/src/Backend/SensorLogger.py
+++ pi-weather/src/Backend/SensorLogger.py
@@ -1,4 +1,4 @@
-#!/usr/bin python3
+#!/usr/bin/python 
 #important line for running in the background
 
 import os
@@ -11,12 +11,12 @@ DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 # function to add to JSON 
-def write_json(data, filename='./Logs/data.json'): 
+def write_json(data, filename='./src/Backend/Logs/data.json'): 
     with open(filename,'w') as f: 
         json.dump(data, f, indent=4) 
 
 try:
-    if os.stat('./Logs/data.json').st_size == 0:
+    if os.stat('./src/Backend/Logs/data.json').st_size == 0:
             data = { "weather_logs" : []}
             write_json(data)
 except:
@@ -29,13 +29,13 @@ while True:
     # when the temperature and humidity both has been read succesfully, append that data to the data.json file
     # src : https://www.geeksforgeeks.org/append-to-json-file-using-python/
     if humidity is not None and temperature is not None:
-        with open('./Logs/data.json') as json_file: 
+        with open('./src/Backend/Logs/data.json') as json_file: 
             data = json.load(json_file) 
             temp = data['weather_logs'] 
             # python object to be appended 
             y = {"Date" : time.strftime('%d %b %Y %H:%M:%S'),
                  "Temp" : round(temperature,2),
-                 "Humi" : humidity} 
+                 "Humi" : round(humidity,2)} 
         
             # appending data to emp_details  
             temp.append(y) 
