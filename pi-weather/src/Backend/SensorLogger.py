811f63c13db5b2511617e477cc9abeb83a691fa4
Viktor Fredholm <vikfre@student.chalmers.se>
2020-12-30 10:35:39 +0100

    global path variable, data file size controll

--- pi-weather/src/Backend/SensorLogger.py
+++ pi-weather/src/Backend/SensorLogger.py
@@ -6,17 +6,18 @@ import time
 import Adafruit_DHT
 import json
 
+filePath = '/piWeather/pi-weather/src/Backend/Logs/data.json'
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
 # function to add to JSON 
-def write_json(data, filename='./src/Backend/Logs/data.json'): 
+def write_json(data, filename=filePath): 
     with open(filename,'w') as f: 
         json.dump(data, f, indent=4) 
 
 try:
-    if os.stat('./src/Backend/Logs/data.json').st_size == 0:
+    if os.stat(filePath).st_size == 0:
             data = { "weather_logs" : []}
             write_json(data)
 except:
@@ -26,10 +27,18 @@ except:
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
+    # If the file size is more than 500KB, removes the oldest data entrys
+    while (os.path.getsize(filePath)>500000):
+        with open(filePath) as file:
+            data = json.load(file)
+            temp = data['weather_logs']
+            data.pop(0)
+        write_json(data)
+
     # when the temperature and humidity both has been read succesfully, append that data to the data.json file
     # src : https://www.geeksforgeeks.org/append-to-json-file-using-python/
     if humidity is not None and temperature is not None:
-        with open('./src/Backend/Logs/data.json') as json_file: 
+        with open(filePath) as json_file: 
             data = json.load(json_file) 
             temp = data['weather_logs'] 
             # python object to be appended 
